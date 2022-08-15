544c833b191eac03ec3c4fb2f16a5ad9f2edb2a2
Matheus Sampaio <matheuslemos98@gmail.com>
2022-08-15 11:08:44 -0300

    Criação Cadastro Cliente

---
+++ CadastroCliente.java
@@ -0,0 +1,7 @@
+public class CadastroCliente {
+    public static void main(String[] args) {
+        //Teste de Cadastro de cliente básico
+
+        String[] informacoes = new String[4];
+    }
+}
